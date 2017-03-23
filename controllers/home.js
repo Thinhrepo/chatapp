a12a6f1f49b7c46c7690524a9ee86b612723b656
thinhnv <thinhnv@vega.com.vn>
2017-03-23 16:33:17 +0700

    fd

---
+++ controllers/home.js
@@ -0,0 +1,44 @@
+const config = require('../config/config.json');
+var io = require('socket.io-client');
+var socket = io.connect(config.server);
+socket.on('connect', function() {console.log('Connected');})
+    .on('connect_error', function() {
+      console.log('Conn error');
+      process.exit(1);
+    })
+    .on('disconnect', function() {console.log('Disconnected'); });
+
+socket.on('message', function (data) {
+  console.log(data);
+});
+
+/**
+ * GET /
+ * Home page.
+ */
+exports.index = (req, res) => {
+  res.render('home', {
+    title: 'Home'
+  }, function (err, result) {
+    res.send(result);
+    socket.emit('join', {name: randomString(8)});
+  });
+};
+
+exports.chat = (req, res) => {
+  socket.emit('join', {name: 'Thinhnv'});
+  res.render('chat', {
+    title: 'chat'
+  });
+};
+
+function randomString(len) {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  var string_length = len;
+  var randomstring = '';
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum,rnum+1);
+  }
+  return randomstring;
+}
